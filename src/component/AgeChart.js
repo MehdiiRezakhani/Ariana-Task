1c2698a122b018a2beca2d508f7548594d81c9e7
MehdiRezakhani <mehdiirezakhani@gmail.com>
2022-11-15 00:49:55 +0330

    the Pie chart is added for skills

--- src/component/AgeChart.js
+++ src/component/AgeChart.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Chart from "chart.js/auto";
 import { Bar } from "react-chartjs-2";
 
 const AgeChart = ({ageData}) => {
--- src/component/SkillsChart.js
+++ src/component/SkillsChart.js
@@ -1,6 +1,5 @@
 import React from "react";
-import Chart from "chart.js/auto";
-import { Bar } from "react-chartjs-2";
+import { Pie } from "react-chartjs-2";
 
 const SkillsChart = ({skillsData}) => {
     // create labels and counts from this object
@@ -16,16 +15,31 @@ const SkillsChart = ({skillsData}) => {
         datasets: [
         {
             label: "Skills Chart",
-            backgroundColor: "rgb(255, 99, 132)",
-            borderColor: "rgb(255, 99, 132)",
             data: skillsCounts,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+              ],
+              borderWidth: 1,
         },
         ],
     };
 
     return (
         <div>
-            <Bar data={data} />
+            <Pie data={data} />
         </div>
     );
 };
